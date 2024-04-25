9105fa86fe40e58e80e2b7fbc3281fe113f14b61
Samed Deniz <155846623+sameddeniz@users.noreply.github.com>
2024-04-25 19:42:51 +0300

    footer change

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
       <footer className="py-5 bg-dark">
         <div className="container">
           <p className="m-0 text-center text-white">
-            Copyright &copy; Your Website 2023
+            Copyright &copy; Samed Deniz 2024
           </p>
         </div>
       </footer>
